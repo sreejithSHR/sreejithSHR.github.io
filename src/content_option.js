fcf090a006c40543df2e097be05750878b423949
Sreejith.R <72004869+sreejithSHR@users.noreply.github.com>
2023-05-14 00:10:52 +0530

    Portfolio

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "Sreejith";
+const meta = {
+    title: "Sreejith_SHR",
+    description: "This is Sreejith, I' a creative and innovative 3d generalistm ",
+};
+
+const introdata = {
+    title: "This is Sreejith_SHR",
+    animated: {
+        first: "Suit yourself",
+        second: "imaginations that exceeds",
+        third: "Welcome",
+    },
+    description: "3D Generalist, Modeller, Game developer, VR Developer, Machine learning and Data science Enthusiast",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: " With 3+ years of experience in computer graphics designing and development, bringing forth the ability to design complex 3D elements, edit static images to produce the illusion of dimension, and create captivating special effects. Adept at working independently and collaboratively on projects, and committed to achieving visual innovation and beauty..",
+};
+const worktimeline = [{
+        jobtitle: "3D generalist",
+        where: "House of MetaZord",
+        date: "2022-23",
+    },
+    {
+        jobtitle: "Graphic designer",
+        where: "Apreture Works",
+        date: "2021-2022",
+    },
+    {
+        jobtitle: "AR/VR designer",
+        where: "Novitech R&D",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Blender",
+        value: 95,
+    },
+    {
+        name: "Unreal Engine",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Unity",
+        value: 60,
+    },
+    {
+        name: "HTML & CSS",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "3D Modeller",
+        description: "Able to create highly realestic 3d model from scratch with simple references in blender with close to photorealistic textures and lighting",
+    },
+	 {
+        title: "Virtual Tour",
+        description: "Interactive Virtual tour for business places and merchants in order to give full experience of the place in any media and easy for client to work.",
+    },
+    {
+        title: "Environment Ceation",
+        description: "Interactive and visually appealing environments that are digital twin of an place or an area with both blend of Ai technologies and human work to produce maximum result.",
+    },
+    {
+        title: "VR Development",
+        description: "Developing realistic and interactive VR application that can be expanded beyond the boundaries.",
+    },
+	 {
+        title: "Machine learning",
+        description: "Providing application to complex computionational problems with machine learning methods and techniques which include Learning each and every information in the data.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdna.artstation.com/p/assets/video_clips/images/057/545/770/smaller_square/sreejith-shr-thumb.jpg?1671903147",
+        description: "last Day on earth.",
+        link: "https://www.artstation.com/artwork/NyZ0WJ",
+    },
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/054/961/191/large/sreejith-shr-y0001.jpg?1665770048",
+        description: "Dead Calm.",
+        link: "https://www.artstation.com/artwork/vJ6kZd",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Re-Evolve.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Beach VR Experiance.",
+        link: "#",
+    },
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/051/615/065/large/sreejith-shr-mues.jpg?1657733488",
+        description: "Leviathan Exhibit.",
+        link: "https://www.artstation.com/artwork/xY4l1W",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/049/645/542/large/sreejith-shr-god.jpg?1652968817",
+        description: "True Titan",
+        link: "https://www.artstation.com/artwork/KOBYrx",
+    },
+
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/062/791/120/large/sreejith-shr-hjk.jpg?1683952517",
+        description: "Cabin In The End.",
+        link: "https://www.artstation.com/artwork/elQy8D",
+    },
+    {
+        img: "https://scontent.fmaa2-4.fna.fbcdn.net/v/t42.1790-2/327792155_210627264849378_3183870373025369049_n.mp4?_nc_cat=105&ccb=1-7&_nc_sid=985c63&efg=eyJybHIiOjM2NSwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=yQOOEyiMAGcAX_GCh7c&rl=365&vabr=203&_nc_ht=scontent.fmaa2-4.fna&oh=00_AfAKChlog7BBaKB7Oc9oP7niOdbY98PUpghDNrlfbYl7Cg&oe=6464AEEA",
+        description: "The Portal.",
+        link: "https://devpost.com/software/paradise-5lh4wv",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Shrsree341o@gmail.com",
+    YOUR_FONE: "+91 88381 90703",
+    description: "Contact for creativity. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+
+const socialprofils = {
+    github: "https://github.com/sreejithSHR",
+    facebook: "https://www.facebook.com/profile.php?id=100081436495254",
+    linkedin: "https://www.linkedin.com/in/sreejith-shr-b05989231/",
+    twitter: "https://twitter.com/ShrSreejith",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
